67c11f3f6c05c33d8eeb00bb7ba9572a3339c201
Saurav Mallik <sauravmallik786@gmail.com>
2023-09-22 10:30:04 +0530

    added

--- Frontend/src/Routes/Routers.jsx
+++ Frontend/src/Routes/Routers.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Pages/Home'
+import Login from './../Pages/Login';
+import Signup from './../Pages/Signup';
+import Doctors from './../Pages/Doctors/Doctors';
+import DoctorDetails from '../Pages/Doctors/DoctorDetails';
+import Contact from './../Pages/Contact';
+import Services from './../Pages/Services';
 
 const Routers = () => {
   return (
-    <div>
-      Routes
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Signup />} />
+      <Route path='/doctors' element={<Doctors />} />
+      <Route path='/doctors/:id' element={<DoctorDetails />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/service' element={<Services />} />
+    </Routes>
   )
 }
 
