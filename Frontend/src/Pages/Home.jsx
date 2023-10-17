3bec603d2f54f39191d7fc1fae9638e67598f693
Saurav Mallik <sauravmallik786@gmail.com>
2023-10-17 14:19:09 +0530

    working on hero section

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import heroImg from '../assets/images/D22.png'
+import heroImg02 from '../assets/images/hero-img02.png'
+import heroImg03 from '../assets/images/hero-img03.png'
 
 const Home = () => {
   return (
@@ -22,6 +25,47 @@ const Home = () => {
                     Assumenda quasi deserunt ipsa quae!
                   </p>
                   <button className="btn">Request an Appointment</button>
+                </div>
+
+                {/* ===================HERO COUNTER====================== */}
+                <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      20+
+                    </h2>
+                    <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px] ">
+                    </span>
+                    <p className="text_para">Years of Experience</p>
+                  </div>
+
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      25+
+                    </h2>
+                    <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px] ">
+                    </span>
+                    <p className="text_para">Clinic Location</p>
+                  </div>
+
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      100%
+                    </h2>
+                    <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px] ">
+                    </span>
+                    <p className="text_para">Patients Satisfaction</p>
+                  </div>
+                </div>
+              </div>
+
+              {/* ======================HERO CONTENT======================= */}
+              <div className="flex gap-[30px] justify end">
+                <div>
+                  <img className="w-full" src={heroImg} alt='' />
+                </div>
+                <div className="mt-[30px]">
+                <img className="w-full mb-[30px]" src={heroImg02} alt='' />
+                <img className="w-full" src={heroImg03} alt='' />
                 </div>
               </div>
             </div>
