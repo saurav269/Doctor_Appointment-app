71753add8f4a97e895dc6ffa9f9d209dc726a0f2
Saurav Mallik <sauravmallik786@gmail.com>
2024-01-30 18:18:06 +0530

    working on backend section

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -1,70 +1,74 @@
+const Doctor = require("../models/DoctorSchema");
 
-
-const Doctor = require("../models/DoctorSchema")
-
-//GET SINGLE USER
-const getSingleDoctor = async(req,res) =>{
-    const id = req.params.id
-    try{
-        const doctor = await Doctor.findById(id);
-        res.status(200).json({status: true, message: "Doctor Found", data: doctor});
-
-    }catch(err){
-        res.status(404).json({status: false, message: "No doctor found"}); 
-
-    }
-}
+//GET SINGLE DOCTOR
+const getSingleDoctor = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const doctor = await Doctor.findById(id)
+      .populate("reviews")
+      .select("-password");
+    res
+      .status(200)
+      .json({ status: true, message: "Doctor Found", data: doctor });
+  } catch (err) {
+    res.status(404).json({ status: false, message: "No doctor found" });
+  }
+};
 
 //GET ALL doctor
-const getAllDoctor = async(req,res) =>{
-    let doctors;
-    try{
-        const {query} = req.query;
-         if(query){
-            doctors = await Doctor.find({
-                isApproved : "approved",
-                $or: [
-                    {name : {$regex : query, $options: "i"}},
-                    {specialization: {$regex:query, $options: "i"}},
-                ],
-             });
-         }else{
-            doctors = await Doctor.find({isApproved: "approved"})
-         }
-        res.status(200).json({status: true, message: "Doctors Found", data: doctors});
-
-    }catch(err){
-        res.status(404).json({status: false, message: "Not found"}); 
-
+const getAllDoctor = async (req, res) => {
+  let doctors;
+  try {
+    const { query } = req.query;
+    if (query) {
+      doctors = await Doctor.find({
+        isApproved: "approved",
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { specialization: { $regex: query, $options: "i" } },
+        ],
+      }).select("-password")
+    } else {
+      doctors = await Doctor.find({ isApproved: "approved" }).select("-password");
     }
-}
-
+    res
+      .status(200)
+      .json({ status: true, message: "Doctors Found", data: doctors });
+  } catch (err) {
+    res.status(404).json({ status: false, message: "Not found" });
+  }
+};
 
 //UPDATE Doctor
-const updateDoctor = async(req,res) =>{
-    const id = req.params.id
-    try{
-        const updateDoctor = await Doctor.findByIdAndUpdate(id,{$set: req.body}, {new:true});
-        res.status(200).json({status: true, message: "Successfully Updated", data: updateDoctor});
-
-    }catch(err){
-        res.status(500).json({status: false, message: "failed Updated"}); 
-
-    }
-}
+const updateDoctor = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updateDoctor = await Doctor.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+    res
+      .status(200)
+      .json({
+        status: true,
+        message: "Successfully Updated",
+        data: updateDoctor,
+      });
+  } catch (err) {
+    res.status(500).json({ status: false, message: "failed Updated" });
+  }
+};
 
 //DELETE USER
-const deleteDoctor = async(req,res) =>{
-    const id = req.params.id
-    try{
-        await Doctor.findByIdAndDelete(id);
-        res.status(200).json({status: true, message: "Successfully deleted"});
+const deleteDoctor = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Doctor.findByIdAndDelete(id);
+    res.status(200).json({ status: true, message: "Successfully deleted" });
+  } catch (err) {
+    res.status(500).json({ status: false, message: "failed to delete" });
+  }
+};
 
-    }catch(err){
-        res.status(500).json({status: false, message: "failed to delete"}); 
-
-    }
-
-}
-
-module.exports = {updateDoctor, getAllDoctor, getSingleDoctor, deleteDoctor}
+module.exports = { updateDoctor, getAllDoctor, getSingleDoctor, deleteDoctor };
---
+++ Backend/models/ReviewSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const reviewSchema = new mongoose.Schema({
+    doctor : {
+        type : mongoose.Types.ObjectId,
+        ref: "Doctor",
+    },
+
+    user : {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    },
+
+    reviewText : {
+        type : String,
+        required: true,
+    },
+
+    rating : {
+        type : Number,
+        required: true,
+        min: 0,
+        max: 5,
+        default : 0,
+    },
+},{
+    timestamps: true
+});
+
+reviewSchema.pre(/^find/, function(next) {
+    this.populate({
+        path: "user",
+        select: "name photo",
+    });
+    next()
+});
+
+module.exports = mongoose.model("Review", reviewSchema)
