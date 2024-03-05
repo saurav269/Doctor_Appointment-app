6cbad99d51348965dc2dcd0038ad68635662e99c
Saurav Mallik <sauravmallik786@example.com>
2024-03-06 00:22:12 +0530

    working on dashboard

--- Frontend/src/Dashboard/doctor-account/DashBoard.jsx
+++ Frontend/src/Dashboard/doctor-account/DashBoard.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Loader from '../../Components/Loader/Loading'
+import useFetch from '../../Hooks/useFetch'
+import { BASE_URL } from '../../config'
+import Error from '../../Components/Error/Error'
+import Tabs from './Tabs'
 
 const DashBoard = () => {
+  const {data, loading, error} = useFetch(`${BASE_URL}/doctors/profile/me`)
+
+  const [tab,setTab] = useState('overview')
+
   return (
-    <div>
-      Dashboard
-    </div>
+  <section>
+      <div className='max-w-[1170px] px-5 mx-auto'>
+       {loading && !error && <Loader />}
+       {error && !loading && <Error />}
+       {!loading && !error && (
+        <div className='grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]'>
+          <Tabs tab={tab} setTab={setTab} />
+        </div>
+       )}
+      </div>
+  </section>
   )
 }
 
