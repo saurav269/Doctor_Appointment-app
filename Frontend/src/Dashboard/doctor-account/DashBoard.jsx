16094d7b1538ffcce149ba99cb6d5668488f7f87
Saurav Mallik <sauravmallik786@example.com>
2024-03-09 12:52:19 +0530

    working on dashboard

--- Frontend/src/Dashboard/doctor-account/DashBoard.jsx
+++ Frontend/src/Dashboard/doctor-account/DashBoard.jsx
@@ -4,6 +4,9 @@ import useFetch from "../../Hooks/useFetch";
 import { BASE_URL } from "../../config";
 import Error from "../../Components/Error/Error";
 import Tabs from "./Tabs";
+import StarIcon from '../../assets/images/Star.png'
+import DoctorAbout from "../../Pages/Doctors/DoctorAbout";
+import Profile from "./Profile";
 
 const DashBoard = () => {
   const { data, loading, error } = useFetch(`${BASE_URL}/doctors/profile/me`);
@@ -42,7 +45,7 @@ const DashBoard = () => {
               )}
 
               <div className="mt-8">
-                {tab === 'overview' && <div>
+                {(tab === 'overview' && <div>
                   <div className=" flex items-center gap-4 mb-10">
                     <figure className=" max-w-[200px] max-h-[200]"><img src={data?.photo} alt='' className=" w-full" /></figure>
 
@@ -50,11 +53,22 @@ const DashBoard = () => {
                       <span className=" bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">Surgeon</span>
 
                       <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">Jhinuk</h3>
+                      <div className=" flex items-center gap-[6px]">
+                        <span className=" flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+                          <img src={StarIcon} alt='' />
+                          4.5
+                        </span>
+                        <span className=" text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+                          (233)
+                        </span>
+                      </div>
+                      <p className=" text__para font-[15px] lg:max-w-[390px] leading-6">Doctor bio</p>
                     </div>
                   </div>
-                  </div>}
+                  <DoctorAbout name={data.name} about={data.about} qualifications={data.qualifications} experiences={data.experiences} />
+                  </div>)} 
                 {tab === 'appointments' && <div>appointments</div>}
-                {tab === 'settings' && <div>Profile settings</div>}
+                {tab === 'settings' && <div><Profile /></div>}
               </div>
 
             </div>
--- Frontend/src/Pages/Doctors/DoctorAbout.jsx
+++ Frontend/src/Pages/Doctors/DoctorAbout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { formatDate } from "../../utilis/formatDate";
 
-const DoctorAbout = () => {
+const DoctorAbout = ({name, about, qualifications, experiences}) => {
   return (
     <div>
       <div>
         <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
           About of
           <span className="text-irisBlueColor font-bold text-[24px] leading-9">
-            Saurav Mallik
+            Jhinuk
           </span>
         </h3>
         <p className="text__para">
